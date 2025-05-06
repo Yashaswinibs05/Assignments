207f6d1c832f575b96b75dbd120ade520ecbf483
Yashaswini <yashas1125@gmail.com>
2025-05-06 16:51:14 +0530

    Assignment14

---
+++ src/assignment14/Assignment14.java
@@ -0,0 +1,45 @@
+package assignment14;
+
+public class Assignment14 {
+
+	public static void main(String[] args) {
+		
+		int n =5;
+		// upper pyramid//
+		 //using numbers 1 to 5 in pyramid//
+		for ( int i=1;i<=n; i++) {
+			
+			//print spaces//
+			
+			for (int j=n; j> i;j-- ) {
+				System.out.print(" ");
+				}
+			// print numbers//
+			
+			for (int k =1; k<=i;k++) {
+				System.out.print(k+ " ");
+			}
+			// go to next line//
+			System.out.println();
+		}
+		// lower pyramid//
+				// numbers from  1 to 5 in pyramid
+				 for (int i= n-1; i>=1;i --) {
+					 // print spaces//
+					 for ( int j=n;j> i;j--) {
+						 System.out.print(" ");
+						 
+						 }
+					// print numbers//
+					 for ( int k=1;k<=i; k++) {
+						 System.out.print(k+ " ");
+						 }
+					 
+					 // go to next line//
+					 System.out.println();
+					 }
+	}
+}
+
+
+
